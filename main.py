1a9ee9d39c68f85df6df77f1523111932f806290
Furdox @ CÃ‰GEP <nt879879@gmail.com>
2024-08-23 10:49:39 -0400

    wpdwarning

---
+++ main.py
@@ -0,0 +1,22 @@
+miaou = "miaou :3"
+print("miaou :3")
+
+print("\n")
+
+print("################################")
+print("#                              #")
+print("#           miaou :3           #")
+print("#                              #")
+print("################################")
+
+print("\n")
+
+print("miaou " + miaou + " :3")
+
+sex1 = input("you know who else? ")
+sex2 = input("my mom! ")
+
+sex = sex1 + sex2
+
+# string
+print(sex1 + " " + sex2)
